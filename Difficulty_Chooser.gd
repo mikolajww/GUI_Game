b90e1f96c6ab2f31333306772ab742307e19b585
Mikołaj Wojtysiak-Wawrzyniak <mikolaj.w.w17@gmail.com>
2019-01-29 21:16:14 +0100

    Added Difficulty Choosing

---
+++ Difficulty_Chooser.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func choose_difficulty(difficulty):
+	Globals.difficulty = difficulty
+	match difficulty:
+		"easy":
+			Globals.initalDifficulty = 2.0
+			Globals.difficultyIncrement = 0.05
+		"normal":
+			Globals.initalDifficulty = 1.5
+			Globals.difficultyIncrement = 0.1
+		"hard":
+			Globals.initalDifficulty = 1.0
+			Globals.difficultyIncrement = 0.15
+	get_tree().change_scene("res://World.tscn")
+
+func _on_Easy_pressed():
+	choose_difficulty("easy")
+
+func _on_Normal_pressed():
+	choose_difficulty("normal")
+
+func _on_Hard_pressed():
+	choose_difficulty("hard")
