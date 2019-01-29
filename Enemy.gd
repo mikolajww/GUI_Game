81ec9c48c43dc138ee78fcca8e25b65dbc95dda5
Mikołaj Wojtysiak-Wawrzyniak <mikolaj.w.w17@gmail.com>
2019-01-29 10:53:13 +0100

    Added Enemies

---
+++ Enemy.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+const textures = [preload("res://enemy_64.png")]
+const BULLET = preload("res://Bullet.tscn")
+
+func _ready():
+	randomize()
+	$KinematicBody2D/Sprite.texture = textures[randi()%(textures.size())]
+	pass
+
+func destroy():
+	queue_free()
+
+func fire():
+	var bullet_right = BULLET.instance()
+	bullet_right.global_position = $KinematicBody2D/Gun_Right.global_position
+	bullet_right.velocity = Vector2(bullet_right.velocity.x, - bullet_right.velocity.y)
+	bullet_right.get_node("Sprite").flip_h = true
+	get_parent().add_child(bullet_right)
+	var bullet_left = BULLET.instance()
+	bullet_left.global_position = $KinematicBody2D/Gun_Left.global_position
+	bullet_left.velocity = Vector2(bullet_left.velocity.x, - bullet_left.velocity.y)
+	bullet_left.get_node("Sprite").flip_h = true
+	get_parent().add_child(bullet_left)
+	
+func _on_Timer_timeout():
+	fire()
