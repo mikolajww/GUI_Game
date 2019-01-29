d48d52205dc4a4663fdbd8ceb51bea3c980f2116
Mikołaj Wojtysiak-Wawrzyniak <mikolaj.w.w17@gmail.com>
2019-01-29 21:09:28 +0100

    Fixed bullet behaviour

--- Bullet.gd
+++ Bullet.gd
@@ -1,18 +1,19 @@
 extends KinematicBody2D
 
-export (Vector2) var velocity = Vector2(0, -50)
+export (Vector2) var velocity = Vector2(0, -15)
 signal _bullet_hit()
 
 func _ready():
 	#player bullet
-	
 	if(get_parent().name == "World"):
+		self.set_collision_layer_bit(2,1)
+		self.set_collision_mask_bit(1,1)
 		connect("_bullet_hit", get_parent().get_node("Counter"), "_on_bullet_hit")
 	#enemy bullet
 	if(get_parent().name == "Enemies"):
+		self.set_collision_layer_bit(5,1)
 		self.set_collision_mask_bit(3,1)
 		connect("_bullet_hit", get_parent().get_parent().get_node("Counter"), "_on_enemy_bullet_hit")
-	print(collision_mask)
 
 func _process(delta):
 	move_and_slide(velocity)
